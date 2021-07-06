f20bb86ab013f3f1b9da473f00358344e4a6de6d
Miguel Grinberg <miguel.grinberg@gmail.com>
2021-07-06 17:20:56 +0100

    first release

---
+++ public/index.js
@@ -0,0 +1,87 @@
+const showGlasses = document.getElementById('glasses');
+const showHat = document.getElementById('hat');
+
+// load the sprites
+const glasses = new Image();
+glasses.src = 'glasses.png';
+const hat = new Image();
+hat.src = 'hat.png';
+
+// for alignment purposes, for each sprite we need to have the pixel location
+// where the right eye (left on the image) goes. The other eye is assumed to
+// be in a simmetrical location on the other half of the image.
+const glassesAlign = {x: 80, y: 110};
+const hatAlign = {x: 115, y: 245};
+
+const showLocalVideo = async () => {
+  // start a local video track
+  const videoTrack = await Twilio.Video.createLocalVideoTrack({
+    width: 640,
+    height: 480,
+    frameRate: 15 
+  });
+  document.getElementById('video').appendChild(videoTrack.attach());
+
+  // align the canvas context with the eye locations returned by the face detector
+  const align = (context, face) => {
+    const rightEye = {x: face.landmarks[0][0], y: face.landmarks[0][1]};
+    const leftEye = {x: face.landmarks[1][0], y: face.landmarks[1][1]};
+
+    // the (0,0) point is going to be the center point between the eyes
+    const origin = {x: (rightEye.x + leftEye.x) / 2, y: (rightEye.y + leftEye.y) / 2}
+    context.translate(origin.x, origin.y);
+
+    // calculate the angle of the line between the eyes with respect to the horizontal
+    const angle = Math.atan2(origin.y - rightEye.y, origin.x - rightEye.x);
+    context.rotate(angle);
+
+    // return the distance between thw eyes, to be used in scaling the sprites
+    return Math.sqrt((rightEye.x - leftEye.x) * (rightEye.x - leftEye.x) + (rightEye.y - leftEye.y) * (rightEye.y - leftEye.y));
+  };
+
+  // draw the sprite
+  // the eyeDistance value returned by the align function is needed to compute
+  // the scaling factor
+  const drawSprite = (context, sprite, spriteAlign, eyeDistance) => {
+    const scale = eyeDistance / (sprite.width - spriteAlign.x * 2);
+    context.scale(scale, scale);
+    context.drawImage(sprite, -sprite.width / 2, -spriteAlign.y)
+    context.scale(1 / scale, 1 / scale);
+  };
+
+  // initialize tensorflow and blazeface
+  await tf.setBackend('webgl');
+  const model = await blazeface.load();
+
+  // create a Twilio video processor
+  const bg = {
+    processFrame: async (input, output) => {
+      const context = output.getContext('2d');
+      if (context) {
+        // draw the video image
+        context.drawImage(input, 0, 0, input.width, input.height);
+
+        // find the faces
+        const faces = await model.estimateFaces(input, false);
+        if (faces.length) {
+          // align the canvas with the face
+          const eyeDistance = align(context, faces[0]);
+
+          // draw the sprites on top of the image
+          if (hat.complete && showHat.checked)
+            drawSprite(context, hat, hatAlign, eyeDistance);
+          if (glasses.complete && showGlasses.checked)
+            drawSprite(context, glasses, glassesAlign, eyeDistance);
+
+          // reset the transformation matrix
+          context.resetTransform();
+        }
+      }
+    },
+  };
+
+  // attach the video processor to the local video track
+  videoTrack.addProcessor(bg);
+}
+
+showLocalVideo();
